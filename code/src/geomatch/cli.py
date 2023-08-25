318ebc5e5a6330ae8e41f353b5ce22197abedf99
ucyo <cayoglu@me.com>
2023-08-25 13:05:21 +0200

    Unify commands and simplify interface
    - Move `distance` and `delta` to a common group for all commands
    - Update description of `--mongo/--no-mongo` flag
    - Unify output variable naming
    - Merge `plot` into `single` command via `output` option

--- code/src/geomatch/cli.py
+++ code/src/geomatch/cli.py
@@ -13,12 +13,6 @@ from .plot import main as plot_main
 
 
 @click.group()
-def cli():
-    """Geomatch Tool - Analysis of TROPOMI and IASI satellite tracks."""
-    pass
-
-
-@cli.command()
 @click.option(
     "--distance",
     default=20,
@@ -33,9 +27,18 @@ def cli():
     type=click.IntRange(min=0),
     help="Temporal tolerance [min].",
 )
+@click.pass_context
+def cli(ctx, distance, delta):
+    """Geomatch Tool - Analysis of TROPOMI and IASI satellite tracks."""
+    ctx.ensure_object(dict)
+    ctx.obj["distance"] = distance
+    ctx.obj["delta"] = timedelta(minutes=delta)
+
+
+@cli.command()
 @click.option(
     "--percentage",
-    default=0.01,
+    default=1,
     show_default=True,
     type=click.FloatRange(0, 1),
     help="Percentage of IASI data.",
@@ -48,11 +51,17 @@ def cli():
     help="Output json file.",
 )
 @click.option(
-    "--mongo/--no-mongo", show_default=True, default=False, help="Use mongoDB instead."
+    "--mongo/--no-mongo",
+    show_default=True,
+    default=False,
+    help="Spatial selection using MongoDB.",
 )
-def match(distance, delta, percentage, output, mongo):
+@click.pass_context
+def match(ctx, percentage, output, mongo):
     """Run search algorithm in either geomatch or mongo."""
-    delta = timedelta(minutes=delta)
+    distance = ctx.obj["distance"]
+    delta = ctx.obj["delta"]
+
     if mongo:
         click.echo("Using mongo for finding matches.")
         mongo_main(distance, delta, percentage, output=output)
@@ -62,63 +71,36 @@ def match(distance, delta, percentage, output, mongo):
 
 
 @cli.command()
-@click.option(
-    "--distance",
-    default=20,
-    show_default=True,
-    type=click.FloatRange(min=0, max=6371),
-    help="Spatial tolerance [km].",
-)
-@click.option(
-    "--delta",
-    default=120,
-    show_default=True,
-    type=click.IntRange(min=0),
-    help="Temporal tolerance [min].",
-)
 @click.option("--id", "ident", default=None, type=str, help="Tropomi ID")
 @click.option(
-    "--ix", default=0, type=click.IntRange(min=0), help="Index position in Tropomi DB"
+    "--ix",
+    default=0,
+    type=click.IntRange(min=0),
+    show_default=True,
+    help="Index position in Tropomi DB",
 )
-def single(distance, delta, ident, ix):
+@click.option(
+    "--output",
+    default=None,
+    show_default=True,
+    type=click.Path(exists=False),
+    help="Output html file.",
+)
+@click.pass_context
+def single(ctx, ident, ix, output):
     """Search single TROPOMI entries w/ geomatch and mongodb."""
-    delta = timedelta(minutes=delta)
+    distance = ctx.obj["distance"]
+    delta = ctx.obj["delta"]
     query = None
+
     if ident:
         query = {"_id": ObjectId(ident)}
         ix = 0
-    geomatch_main(distance, delta, ix, query=query)
-
-
-@cli.command()
-@click.option(
-    "--distance",
-    default=20,
-    show_default=True,
-    type=click.FloatRange(min=0, max=6371),
-    help="Spatial tolerance [km].",
-)
-@click.option(
-    "--delta",
-    default=120,
-    show_default=True,
-    type=click.IntRange(min=0),
-    help="Temporal tolerance [min].",
-)
-@click.option("--id", "ident", default=None, type=str, help="Tropomi ID")
-@click.option(
-    "--ix", default=0, type=click.IntRange(min=0), help="Index position in Tropomi DB"
-)
-@click.option("--file", default=None, help="Output html file.")
-def plot(distance, delta, ident, ix, file):
-    """Plot a TROPOMI entry and compare geomatch/mongo."""
-    delta = timedelta(minutes=delta)
-    query = None
-    if ident:
-        query = {"_id": ObjectId(ident)}
-        ix = 0
-    plot_main(distance, delta, ix, query=query, save=file)
+    if output is not None:
+        plot_main(distance, delta, ix, query=query, save=output)
+    else:
+        geomatch_main(distance, delta, ix, query=query)
 
 
 def main():
-    cli()
+    cli(obj={})
